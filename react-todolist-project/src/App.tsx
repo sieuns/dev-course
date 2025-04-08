540bd257f10ce9e530214364a4b167aaa2fe25c8
양시은 <mn07020@gmail.com>
2025-04-08 20:22:55 +0900

    React로 todolist 연습

---
+++ react-todolist-project/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import TodoList from './Todolist';
+import Clock from './Timer';
+import MyWeather from './MyWeather';
+
+function App() {
+  return (
+    <div className="container">
+      <TodoList></TodoList>
+      {/*<Clock></Clock>*/}
+      <MyWeather weather ='맑음'>일기예보</MyWeather>
+    </div>
+  );
+}
+
+export default App;
