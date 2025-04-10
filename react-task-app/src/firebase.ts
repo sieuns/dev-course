815e4340ac1e364f3491d27d35669b71f9abc62e
양시은 <mn07020@gmail.com>
2025-04-10 18:30:15 +0900

    feat: Firebase 초기 설정

---
+++ react-task-app/src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBftXJ9Sex8zy064DMpGeGQCHRdL1dZ7ZA",
+  authDomain: "react-task-app-845b0.firebaseapp.com",
+  projectId: "react-task-app-845b0",
+  storageBucket: "react-task-app-845b0.firebasestorage.app",
+  messagingSenderId: "766692405298",
+  appId: "1:766692405298:web:856a5fb2861e57301d8c2b",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
