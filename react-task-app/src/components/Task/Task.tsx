c1e774c0ff45ba84d106cce9df22cc04e08df9b3
양시은 <mn07020@gmail.com>
2025-04-10 18:06:48 +0900

    feat: Task 컴포넌트 구현 및 드래그 기능 구현

---
+++ react-task-app/src/components/Task/Task.tsx
@@ -0,0 +1,37 @@
+import { FC } from "react";
+import { container, description, title } from "./Task.css";
+import { Draggable } from "@hello-pangea/dnd";
+
+type TTaskProps = {
+  index: number;
+  id: string;
+  boardId: string;
+  taskName: string;
+  taskDescription: string;
+};
+
+const Task: FC<TTaskProps> = ({
+  index,
+  id,
+  // boardId,
+  taskName,
+  taskDescription,
+}) => {
+  return (
+    <Draggable draggableId={id} index={index}>
+      {(provided) => (
+        <div
+          className={container}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <div className={title}>{taskName}</div>
+          <div className={description}>{taskDescription}</div>
+        </div>
+      )}
+    </Draggable>
+  );
+};
+
+export default Task;
