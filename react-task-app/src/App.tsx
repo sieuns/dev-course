abe86ef0c536f633b99caa572bb5138cbf2018dc
양시은 <mn07020@gmail.com>
2025-04-10 18:32:08 +0900

    feat: 메인 페이지(App) 레이아웃 및 기능 구현

--- react-task-app/src/App.tsx
+++ react-task-app/src/App.tsx
@@ -1,35 +1,130 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {
+  appContainer,
+  board,
+  buttons,
+  deleteBoardButton,
+  loggerButton,
+} from "./App.css.ts";
+import BoardList from "./components/BoardList/BoardList";
+import ListsContainer from "./components/ListsContainer/ListsContainer";
+import { useTypedDispatch, useTypedSelector } from "./hooks/redux";
+import EditModal from "./components/EditModal/EditModal";
+import LoggerModal from "./components/LoggerModal/LoggerModal";
+import { deleteBoard, sort } from "./store/slices/boardsSlice";
+import { addLog } from "./store/slices/loggerSlice";
+import { v4 } from "uuid";
+import { DragDropContext } from "@hello-pangea/dnd";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useTypedDispatch();
+  const [isLoggerOpen, setIsLoggerOpen] = useState(false);
+  const [activeBoardId, setActiveBoardId] = useState("board-0");
+  const modalActive = useTypedSelector((state) => state.boards.modalActive);
+  const boards = useTypedSelector((state) => state.boards.boardArray);
+
+  const getActiveBoard = boards.filter(
+    (board) => board.boardId === activeBoardId
+  )[0];
+
+  const lists = getActiveBoard.lists;
+
+  const handleDeleteBoard = () => {
+    if (boards.length > 1) {
+      dispatch(deleteBoard({ boardId: getActiveBoard.boardId }));
+
+      dispatch(
+        addLog({
+          logId: v4(),
+          logMessage: `게시판 지우기: ${getActiveBoard.boardName}`,
+          logAuthor: "User",
+          logTimestamp: String(Date.now()),
+        })
+      );
+
+      const newIndexToSet = () => {
+        const indexToBeDeleted = boards.findIndex(
+          (board) => board.boardId === activeBoardId
+        );
+
+        return indexToBeDeleted === 0
+          ? indexToBeDeleted + 1
+          : indexToBeDeleted - 1;
+      };
+
+      setActiveBoardId(boards[newIndexToSet()].boardId);
+    } else {
+      alert("최소 게시판 개수는 한 개입니다.");
+    }
+  };
+
+  const handleDragEnd = (result: any) => {
+    const { destination, source, draggableId } = result;
+
+    const sourceList = lists.filter(
+      (list) => list.listId === source.droppableId
+    )[0];
+
+    dispatch(
+      sort({
+        boardIndex: boards.findIndex(
+          (board) => board.boardId === activeBoardId
+        ),
+        droppableIdStart: source.droppableId,
+        droppableIdEnd: destination.droppableId,
+        droppableIndexStart: source.index,
+        droppableIndexEnd: destination.index,
+        draggableId,
+      })
+    );
+
+    dispatch(
+      addLog({
+        logId: v4(),
+        logMessage: `
+                리스트 "${sourceList.listName} "에서
+                리스트 "${
+                  lists.filter(
+                    (list) => list.listId === destination.droppableId
+                  )[0].listName
+                }"으로
+                ${
+                  sourceList.tasks.filter(
+                    (task) => task.taskId === draggableId
+                  )[0].taskName
+                }을 옮김.
+                `,
+        logAuthor: "User",
+        logTimestamp: String(Date.now()),
+      })
+    );
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={appContainer}>
+      {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen} /> : null}
+      {modalActive ? <EditModal /> : null}
+      <BoardList
+        activeBoardId={activeBoardId}
+        setActiveBoardId={setActiveBoardId}
+      />
+      <div className={board}>
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <ListsContainer lists={lists} boardId={getActiveBoard.boardId} />
+        </DragDropContext>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className={buttons}>
+        <button className={deleteBoardButton} onClick={handleDeleteBoard}>
+          이 게시판 삭제하기
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button
+          className={loggerButton}
+          onClick={() => setIsLoggerOpen(!isLoggerOpen)}
+        >
+          {isLoggerOpen ? "활동 목록 숨기기" : "활동 목록 보이기"}
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
+export default App;
