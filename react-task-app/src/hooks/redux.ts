3ce9f926dc6c3a6204610e41b4ae4de37c1fe86f
양시은 <mn07020@gmail.com>
2025-04-10 18:28:47 +0900

    feat: useDispatch, useSelector 커스텀 훅 추가

---
+++ react-task-app/src/hooks/redux.ts
@@ -0,0 +1,5 @@
+import { AppDispatch, RootState } from "../store";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useTypedDispatch = () => useDispatch<AppDispatch>();
