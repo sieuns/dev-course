494646e96469c16f8fb5d60893f38c77190db8a9
양시은 <mn07020@gmail.com>
2025-04-23 13:36:45 +0900

    feat: Detail 페이지 생성

---
+++ book-store/src/pages/Detail.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "../components/common/Sidebar";
+
+function Detail() {
+    return (
+        <>
+            <Sidebar />
+            <div>디테일</div>
+        </>
+       
+    )
+}
+
+export default Detail;
