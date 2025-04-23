bec3d9d0c1d32379cdacf41490ac96445e8db19d
양시은 <mn07020@gmail.com>
2025-04-23 16:56:10 +0900

    feat: Header, Footer 포함된 Layout 컴포넌트 구현

--- book-store/src/components/layout/Layout.tsx
+++ book-store/src/components/layout/Layout.tsx
@@ -1,5 +1,7 @@
+import styled from "styled-components";
 import Footer from "../common/Footer";
 import Header from "../common/Header";
+import { Theme } from "../../style/styled";
 
 interface LayoutProps {
     children : React.ReactNode;
@@ -9,10 +11,17 @@ function Layout({children} : LayoutProps) {
     return (
         <>
             <Header />
-            <main>{children}</main>
+            <LayoutStyle>{children}</LayoutStyle>
             <Footer />
         </>
     )
 }
+
+const LayoutStyle = styled.main<{ theme: Theme }>`
+  width: 100%;
+  margin: 0 auto;
+  max-width: ${({ theme }) => theme.layout.width.large};
+  padding: 20px 0;
+`;
 
 export default Layout;
