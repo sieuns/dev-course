0d641c3af7745eed3ee1a488fcddd141672232ac
양시은 <mn07020@gmail.com>
2025-04-23 16:52:14 +0900

    feat: 푸터 컴포넌트 구현

--- book-store/src/components/common/Footer.tsx
+++ book-store/src/components/common/Footer.tsx
@@ -1,10 +1,38 @@
+import styled from "styled-components";
+import logo from "../../assest/images/logo.png";
+
 function Footer() {
-    return (
-      <>
-          <hr />
-          <div>copyright(c), 2025, book store.</div>
-      </>
-    )
+  return (
+    <FooterStyle>
+      <h1 className="logo">
+        <img src={logo} alt="book" />
+      </h1>
+      <div className="copyright">copyright(c).2025.Book Store.</div>
+    </ FooterStyle>
+  );
+}
+
+const FooterStyle = styled.footer`
+  width: 100%;
+  margin: 0 auto;
+  max-width: ${({ theme }) => theme.layout.width.large};
+  border-top: 1px solid ${({ theme }) => theme.color.background};
+  padding: 20px 0;
+  display: flex;
+  justify-content: space-between;
+
+  .logo {
+    img {
+      width: 140px;
+    }
   }
-  
-  export default Footer;
+
+  .copyright {
+    p {
+      font-size: 0.75rem;
+      color: ${({ theme }) => theme.color.text};
+    }
+  }
+`;
+
+export default Footer;
