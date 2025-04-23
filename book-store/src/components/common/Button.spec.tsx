197f38216278764064bb68c59f0ede11bdbdf8e8
양시은 <mn07020@gmail.com>
2025-04-23 16:54:18 +0900

    feat: Button 컴포넌트 및 테스트 코드 작성

---
+++ book-store/src/components/common/Button.spec.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+import { BookStoreThemeProvider } from "../../context/themeContext";
+
+describe("Button 컴포넌트 테스트", () => {
+  it("렌더를 확인", () => {
+    render(
+      <BookStoreThemeProvider>
+        <Button size="large" scheme="primary">
+          버튼
+        </Button>
+      </BookStoreThemeProvider>
+    );
+
+    expect(screen.getByText("버튼")).toBeInTheDocument();
+  });
+
+  it("size props 적용", () => {
+    render(
+      <BookStoreThemeProvider>
+        <Button size="large" scheme="primary">
+          버튼
+        </Button>
+      </BookStoreThemeProvider>
+    );
+
+    expect(screen.getByRole("button")).toHaveStyle({ fontSize: "1.5rem" });
+  });
+
+  it("color props 적용", () => {
+    render(
+        <BookStoreThemeProvider>
+          <Button size='large' scheme='primary'>
+            버튼
+          </Button>
+        </BookStoreThemeProvider>
+      );
+  
+      expect(screen.getByRole('button')).toHaveStyle({ color: 'white' });
+  });
+});
