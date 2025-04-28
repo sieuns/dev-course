1b2526b037ec75bfe86bb2b87406aeda611fde64
양시은 <mn07020@gmail.com>
2025-04-28 22:01:19 +0900

    feat: 도서 상세 페이지 추가

--- book-store/src/components/books/BooksEmpty.tsx
+++ book-store/src/components/books/BooksEmpty.tsx
@@ -1,39 +1,15 @@
-import styled from "styled-components";
 import { FaSmile } from "react-icons/fa";
-import Title from "../common/Title";
 import { Link } from "react-router-dom";
+import Empty from "../common/Empty";
 
 function BooksEmpty() {
   return (
-    <BooksEmptyStyle>
-      <div className="icon">
-        <FaSmile />
-        console.log(FaSmile);
-      </div>
-      <Title size="large" color="secondary">
-        검색 결과가 없습니다.
-      </Title>
-      <p>
-        <Link to="/books">전체 검색 결과로 이동</Link>
-      </p>
-    </BooksEmptyStyle>
+    <Empty
+      title="검색 결과가 없습니다."
+      icon={<FaSmile />}
+      description={<Link to="/books">전체 검색 결과로 이동</Link>}
+    ></Empty>
   );
 }
-
-const BooksEmptyStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 12px;
-  padding: 120px 0;
-
-  .icon {
-    svg {
-      font-size: 4rem;
-      fill: #ccc;
-    }
-  }
-`;
 
 export default BooksEmpty;
