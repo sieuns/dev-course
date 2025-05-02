edb2c9dfc4e7f7a29a84e8fd1d819e70658e378a
양시은 <mn07020@gmail.com>
2025-05-02 16:19:07 +0900

    feat: MSW 설정 및 mock 핸들러 구성

--- book-store/src/index.tsx
+++ book-store/src/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ThemeContext, state } from "./context/themeContext";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function mountApp() {
+  if (process.env.NODE_ENV === "development") {
+    const { worker } = require("./mock/browser");
+    await worker.start();
+  }
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+mountApp();
