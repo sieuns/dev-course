c0a9f460ea9b15762c65c0dcc1ac0fbaf0de9ea1
양시은 <mn07020@gmail.com>
2025-05-02 16:26:30 +0900

    feat: 주문 기능 추가

---
+++ book-store/src/api/order.api.ts
@@ -0,0 +1,14 @@
+import { OrderSheet } from "../models/order.model";
+import { requestHandler } from "./http";
+
+export const order = async (orderData: OrderSheet) => {
+  return await requestHandler<OrderSheet>("post", "/orders", orderData);
+};
+
+export const fetchOrders = async () => {
+  return await requestHandler("get", "/orders");
+};
+
+export const fetchOrder = async (orderId: number) => {
+  return await requestHandler("get", `/orders/${orderId}`);
+};
