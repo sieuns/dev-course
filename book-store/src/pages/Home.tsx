5e6984bdaeb0bed90c0dc6a2105f7fe43fe1835d
양시은 <mn07020@gmail.com>
2025-05-03 23:23:04 +0900

    feat: 메인 페이지에서 리뷰, 신간, 베스트셀러, 배너 호출

--- book-store/src/pages/Home.tsx
+++ book-store/src/pages/Home.tsx
@@ -1,13 +1,39 @@
+import Title from "@/components/common/Title";
+import Banner from "@/components/common/banner/Banner";
+import MainBest from "@/components/main/MainBest";
+import MainNewBooks from "@/components/main/MainNewBooks";
+import MainReview from "@/components/main/MainReview";
+import { useMain } from "@/hooks/useMain";
 import { styled } from "styled-components";
 
 function Home() {
+  const { reviews, newBooks, bestBooks, banners } = useMain();
   return (
     <HomeStyle>
-      <h1>Home</h1>
+      <Banner banners={banners} />
+
+      <section className="section">
+        <Title size="large">베스트 셀러</Title>
+        <MainBest books={bestBooks} />
+      </section>
+
+      <section className="section">
+        <Title size="large">신간 안내</Title>
+        <MainNewBooks books={newBooks} />
+      </section>
+
+      <section className="section">
+        <Title size="large">리뷰</Title>
+        <MainReview reviews={reviews} />
+      </section>
     </HomeStyle>
   );
 }
 
-const HomeStyle = styled.div``;
+const HomeStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
 
 export default Home;
