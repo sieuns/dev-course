02b4e73573306473e8bff01361a56f97e3e82dc4
양시은 <mn07020@gmail.com>
2025-06-14 16:31:42 +0900

    refactor: 함수 파라미터 타입 수정

--- book-store/src/utils/format.ts
+++ book-store/src/utils/format.ts
@@ -1,9 +1,9 @@
 import dayjs from "dayjs";
 
-export const formatNumber = (number : Number) => {
-    return number.toLocaleString();
+export const formatNumber = (value?: number | null) => {
+  return (value ?? 0).toLocaleString();
 };
 
 export const formatDate = (date: string, format?: string) => {
-    return dayjs(date).format(format ? format : "YYYY년 MM월 DD일");
+  return dayjs(date).format(format ? format : "YYYY년 MM월 DD일");
 };
